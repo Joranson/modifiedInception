8e1be9808c6006387318efba19096f0d3c52a86d
Qiyin Wu <qiyin.wu.jor@gmail.com>
2017-05-02 00:41:37 -0700

    break loop even not a chief

--- inception/inception/inception_distributed_train.py
+++ inception/inception/inception_distributed_train.py
@@ -319,11 +319,12 @@ def train(target, dataset, cluster_spec):
             sess.run(clean_up_op)
           raise
 
-      # Stop the supervisor.  This also waits for service threads to finish.
-      sv.stop()
+      return
+      # # Stop the supervisor.  This also waits for service threads to finish.
+      # sv.stop()
 
-      # Save after the training ends.
-      if is_chief:
-        saver.save(sess,
-                   os.path.join(FLAGS.train_dir, 'model.ckpt'),
-                   global_step=global_step)
+      # # Save after the training ends.
+      # if is_chief:
+      #   saver.save(sess,
+      #              os.path.join(FLAGS.train_dir, 'model.ckpt'),
+      #              global_step=global_step)
